1376bab9f7b2db012b862fee4c1ecea820d02e35
Utz Nisslmueller <utzn42@univie.ac.at>
2020-01-10 11:21:55 +0100

    Basic Flask Server

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/login')
+def home():
+    errors = []
+    results = []
+    try:
+        login_form = request.form["login_data"]
+
+    except:
+        errors.append()
+
+    return render_template("home.html", errors=errors, results=results)
 
 
 if __name__ == '__main__':
